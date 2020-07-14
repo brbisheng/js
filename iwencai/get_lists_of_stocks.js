8f08fa1d16196f927276e7a70593beaad9618927
Sheng BI <brbisheng@gmail.com>
2020-07-15 04:10:59 +0800

    Update get_lists_of_stocks.js

--- iwencai/get_lists_of_stocks.js
+++ iwencai/get_lists_of_stocks.js
@@ -8,3 +8,17 @@ function f0() {
     console.log(b);
     return b;
 }
+
+
+function f1() {
+    // get the tbody
+    var a = document.querySelector('#tableWrap > div.table_templ_box.fixed_atomic_table_con > div > div.scroll_con_outer > div > div > div.scroll_tbody_con > table > tbody');
+    var b = a.children;
+    var result = [];
+    for (let i = 0; i<b.length; i++) {
+        result.push(b[i].children[9].innerText);
+    }
+    return result;
+
+
+}
